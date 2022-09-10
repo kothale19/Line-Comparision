9fbbf529ad672a5adf6c8f7d3b50498ff79ba850
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-10 15:32:02 +0530

    Added: Calculate the Length using Cartesian system Logic

--- src/com/blz/LineComparision/LineComparision.java
+++ src/com/blz/LineComparision/LineComparision.java
@@ -4,7 +4,17 @@ public class LineComparision {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation Program!!");
-
+        int X1 = 2;
+        int Y1 = 3;
+        
+        int X2 = 5;
+        int Y2 = 6;
+        
+       double length = Math.sqrt(Math.pow(X2-X1,2) + Math.pow(Y2-Y1,2));
+	   System.out.println("length of line is: "+length);	
+		
+		
+		
 	}
 
 }
